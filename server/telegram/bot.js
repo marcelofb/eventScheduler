a8afbf3f688577b95f27c519d8a500a1126638bf
Marcelo Burriel <marceloburriel@gmail.com>
2026-05-29 19:56:14 -0300

    add multiple chat_id support

--- server/telegram/bot.js
+++ server/telegram/bot.js
@@ -2,14 +2,18 @@ const axios = require('axios');
 
 async function sendTelegramMessage(text) {
   const token = process.env.TELEGRAM_BOT_TOKEN;
-  const chatId = process.env.TELEGRAM_CHAT_ID;
+  const chatIds = (process.env.TELEGRAM_CHAT_ID || '').split(',').map((id) => id.trim()).filter(Boolean);
   const url = `https://api.telegram.org/bot${token}/sendMessage`;
 
-  await axios.post(url, {
-    chat_id: chatId,
-    text,
-    parse_mode: 'HTML',
-  });
+  await Promise.all(
+    chatIds.map((chatId) =>
+      axios.post(url, {
+        chat_id: chatId,
+        text,
+        parse_mode: 'HTML',
+      })
+    )
+  );
 }
 
 module.exports = { sendTelegramMessage };
